421daa404f8bcba8329b30e334c58600545aca5e
hannesroest@gmail.com <hannesroest@gmail.com@db641661-14cc-1e01-c2f8-e77ac6f8a696>
2013-03-07 15:18:52 +0000

    [TEST] add readme, use unittest

--- trunk/test/test_import.py
+++ trunk/test/test_import.py
@@ -1,1 +1,12 @@
-import msproteomicstoolslib
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_import_msproteomicstoolslib(self):
+        import msproteomicstoolslib
+
+if __name__ == '__main__':
+    unittest.main()
