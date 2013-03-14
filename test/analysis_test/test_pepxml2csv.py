8ff900dc2d4a22575db91b3e53d99afcd6d9893b
Hannes Roest <hroest@users.noreply.github.com>
2013-03-14 14:44:22 +0000

    [TEST] black box testing of script

---
+++ test/analysis_test/test_pepxml2csv.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+"""
+=========================================================================
+        msproteomicstools -- Mass Spectrometry Proteomics Tools
+=========================================================================
+
+Copyright (c) 2013, ETH Zurich
+For a full list of authors, refer to the file AUTHORS.
+
+This software is released under a three-clause BSD license:
+ * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+ * Neither the name of any author or any participating institution
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+--------------------------------------------------------------------------
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+$Maintainer: Hannes Roest$
+$Authors: Hannes Roest$
+--------------------------------------------------------------------------
+"""
+
+import unittest
+import subprocess as sub
+import os
+
+class TestPepXMLReader(unittest.TestCase):
+
+    def setUp(self):
+        self.dirname = os.path.dirname(os.path.abspath(__file__))
+        self.topdir = os.path.join(os.path.join(self.dirname, ".."), "..")
+        self.datadir = os.path.join(os.path.join(self.topdir, "test"), "data")
+        self.scriptdir = os.path.join(self.topdir, "analysis")
+
+    def exact_diff(self, name1, name2):
+        f1 = open(name1, "r")
+        f2 = open(name2, "r")
+        for l1,l2 in zip(f1,f2):
+            self.assertEqual(l1,l2)
+
+    def fuzzy_diff(self):
+        #http://stackoverflow.com/questions/3108274/generate-fuzzy-difference-of-two-files-in-python?lq=1
+        pass
+
+    def test_pepxml2csv(self):
+        script = os.path.join(os.path.join(self.scriptdir, "data_conversion"), "pepxml2csv.py")
+        filename = os.path.join(self.datadir, "test.pep.xml")
+        expected_outcome = os.path.join(self.datadir, "pepxml2csv.expected_out.csv")
+        tmpfilename = "pepxml2csv.out.tmp"
+
+        args = "%s %s" % (filename, tmpfilename)
+        cmd = "python %s %s" % (script, args)
+        p = sub.Popen(cmd,shell=True,stdout=sub.PIPE,stderr=sub.PIPE)
+        output, errors = p.communicate()
+
+        self.exact_diff(tmpfilename, expected_outcome)
+
+        os.remove(tmpfilename)
+
+if __name__ == '__main__':
+    unittest.main()
