f7184d8edb9b232f01210b7fd0e1374c9d4dc642
blum@imsb.biol.ethz.ch <blum@imsb.biol.ethz.ch@db641661-14cc-1e01-c2f8-e77ac6f8a696>
2013-03-20 12:42:11 +0000

    mProphetRunner

--- trunk/analysis/mProphet/mProphetRunner.sh
+++ trunk/analysis/mProphet/mProphetRunner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#put this script in same folder as mProphet.R and then add to PATH to simplify call to mProphet (no path hardcoding required)
+#put this script in same folder as mProphet.R and then add to PATH to simplify mProphet calls (no path hardcoding required)
 export MPROPHET_BINDIR=$(dirname $0)
 set -x
 R --slave --file=$MPROPHET_BINDIR/mProphet.R --args bin_dir=$MPROPHET_BINDIR $@
