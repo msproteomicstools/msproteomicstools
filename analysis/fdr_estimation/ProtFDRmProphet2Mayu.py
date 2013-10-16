7b50541c0a8d487772dffd7ea8042af704237ce1
Hannes Roest <hroest@users.noreply.github.com>
2013-10-16 18:15:36 +0000

    finish mProphet2Mayu

--- analysis/fdr_estimation/ProtFDRmProphet2Mayu.py
+++ analysis/fdr_estimation/ProtFDRmProphet2Mayu.py
@@ -6,16 +6,14 @@
 
 import sys, csv, argparse
 
-import gzip
-
-
 def handle_args():
     usage = "" #usage: %prog --in \"files1 file2 file3 ...\" [options]" 
     usage += "\nThis program converts an mProphet TSV output file to a Mayu input file"
 
     parser = argparse.ArgumentParser(description = usage )
-    parser.add_argument('--in', dest="infiles", required=True, nargs = '+', help = 'A list of mProphet output files containing all peakgroups (use quotes around the filenames)')
+    parser.add_argument('--in', dest="infile", required=True, help = 'A list of mProphet output files containing all peakgroups (use quotes around the filenames)')
     parser.add_argument("--out", dest="outfile", required=True, help="Output file")
+    # parser.add_argument('--use_alignment_id', action='store_true', default=False, help="Use the column 'align_runid' to dermine from which run an identification originated")
 
     args = parser.parse_args(sys.argv[1:])
     return args
@@ -27,35 +25,33 @@ def get_filehandler(inputf):
     else:
         return open(inputf)
 
-args = handle_args()
+def write_output(infile, outfile):
+    for line_nr, line in enumerate(infile):
+        runid = line[ header_dict["align_runid"] ]
+        firstline = "run%s.%s.%s.%s" % (runid, line_nr, line_nr, line[ header_dict["Charge"]  ])
+        newline = [ 
+            firstline,
+            line[ header_dict["Sequence"]  ],
+            line[ header_dict["ProteinName"]  ],
+            "", # no mods
+            1 - float(line[ header_dict["m_score"]  ]),
+        ]
+        outfile.writerow(newline)
 
-outfile = csv.writer(open(args.outfile, "w"), delimiter=",")
+if __name__=="__main__":
+    args = handle_args()
 
-if len(args.infiles) != 1:
-    raise Exception("More than one infile not yet implemented!") 
+    outfile = csv.writer(open(args.outfile, "w"), delimiter=",")
+    outfile.writerow(["Identifier", "Sequence", "Protein", "Mod", "MScore"])
 
-# Input
-inputf = args.infiles[0]
-filehandler = get_filehandler(inputf)
-infile = csv.reader(filehandler, delimiter="\t")
+    # Input
+    inputf = args.infile
+    filehandler = get_filehandler(inputf)
+    infile = csv.reader(filehandler, delimiter="\t")
 
-# Inputheader
-header = infile.next()
-header_dict = dict([(h, i) for i,h in enumerate(header)])
+    # Inputheader
+    header = infile.next()
+    header_dict = dict([(h, i) for i,h in enumerate(header)])
 
-outfile.writerow(["Identifier", "Sequence", "Protein", "Mod", "MScore"])
-line_nr = 0
-for line in infile:
-    line_nr += 1
-    runid = line[ header_dict["align_runid"] ]
-    #trgroup = line[ header_dict["transition_group_record"]  ]
-    firstline = "run%s.%s.%s.%s" % (runid, line_nr, line_nr, line[ header_dict["Charge"]  ])
-    newline = [ 
-        firstline,
-        line[ header_dict["Sequence"]  ],
-        line[ header_dict["ProteinName"]  ],
-        "", # no mods
-        1 - float(line[ header_dict["m_score"]  ]),
-    ]
-    outfile.writerow(newline)
+    write_output(infile, outfile)
 
