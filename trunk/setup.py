35efb0b772a838e390568b6a7b2d12e3e6ef1f37
hannesroest@gmail.com <hannesroest@gmail.com@db641661-14cc-1e01-c2f8-e77ac6f8a696>
2014-01-30 13:51:37 +0000

    [FEATURE] add setup.py

---
+++ trunk/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+import fnmatch
+all_scripts = []
+for root, dirnames, filenames in os.walk('analysis'):
+  for filename in fnmatch.filter(filenames, '*.py'):
+      all_scripts.append(os.path.join(root, filename))
+
+setup(name='msproteomicstools',
+      version='0.1.0',
+      packages = ['msproteomicstoolslib', 
+                  "msproteomicstoolslib.algorithms",
+                  "msproteomicstoolslib.algorithms.alignment",
+                  "msproteomicstoolslib.data_structures",
+                  "msproteomicstoolslib.format",
+                  "msproteomicstoolslib.math",
+                  "msproteomicstoolslib.util",
+                  "openswathgui",
+                  "openswathgui.models",
+                  "openswathgui.views",
+                 ],
+      package_dir = {
+          'openswathgui': 'gui/openswath',
+      },
+      scripts=all_scripts,
+      description='Tools for MS-based proteomics',
+      long_description='msproteomicstools - python module for MS-based proteomics',
+      url='https://code.google.com/p/msproteomicstools',
+      license='Modified BSD',
+      platforms='any that supports python 2.7',
+      )
+
