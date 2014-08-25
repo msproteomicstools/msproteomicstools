ab6c31621364518780b3307343629c9cb464d95b
Hannes Roest <hroest@users.noreply.github.com>
2014-08-25 16:14:17 +0000

    [FEATURE] add Minimal Spanning Tree library code
    - add code from PADS

---
+++ msproteomicstoolslib/algorithms/PADS/MinimumSpanningTree.py
@@ -0,0 +1,30 @@
+"""MinimumSpanningTree.py
+
+Kruskal's algorithm for minimum spanning trees. D. Eppstein, April 2006.
+
+http://www.ics.uci.edu/~eppstein/PADS/.git/ (Public Domain)
+"""
+
+from UnionFind import UnionFind
+
+def MinimumSpanningTree(G):
+    """
+    Return the minimum spanning tree of an undirected graph G.
+    G should be represented in such a way that G[u][v] gives the
+    length of edge u,v, and G[u][v] should always equal G[v][u].
+    The tree is returned as a list of edges.
+    """
+    
+    # Kruskal's algorithm: sort edges by weight, and add them one at a time.
+    # We use Kruskal's algorithm, first because it is very simple to
+    # implement once UnionFind exists, and second, because the only slow
+    # part (the sort) is sped up by being built in to Python.
+    subtrees = UnionFind()
+    tree = []
+    edges = [(G[u][v],u,v) for u in range(len(G)) for v in range(len(G[u]))]
+    edges.sort()
+    for W,u,v in edges:
+        if subtrees[u] != subtrees[v]:
+            tree.append((u,v))
+            subtrees.union(u,v)
+    return tree        
