4f2ed6bde89615c0a4a5a5423d2079fc964b141d
Hannes Roest <roest@imsb.biol.ethz.ch>
2015-08-05 19:16:11 -0700

    [FIX] fix weights if point is closer than minimal distance
    - ensure that the weight is equal to 1/s**e in this case and is
      comparable with the other weights

--- msproteomicstoolslib/math/Smoothing.py
+++ msproteomicstoolslib/math/Smoothing.py
@@ -738,8 +738,8 @@ class WeightedNearestNeighbour(LocalKernel):
             # Use transformed target data to compute expected RT in target domain (weighted average)
             # EXP = 0.65 # produces slightly better results
             EXP = 1.0
-            expected_targ = numpy.average(target_data_transf, weights=[ 1/abs(s) if s > self.min_diff else self.min_diff for s in source_d_diff])
-            expected_targ = numpy.average(target_data_transf, weights=[ 1/(abs(s)**EXP) if s > self.min_diff else self.min_diff for s in source_d_diff])
+            expected_targ = numpy.average(target_data_transf, weights=
+                                          [ 1/(abs(s)**EXP) if s > self.min_diff else 1/(abs(self.min_diff)**EXP) for s in source_d_diff])
 
             # Compute a measurement of dispersion, standard deviation
             self.last_dispersion = numpy.std(target_data_transf)
