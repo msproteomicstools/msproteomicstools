113a05d05f37694163bb2daf722697edc4ac24c2
Hannes Roest <roest@imsb.biol.ethz.ch>
2015-12-23 22:35:17 -0800

    Version 0.4.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
     extra_installs = []
 
 setup(name='msproteomicstools',
-      version='0.3.3',
+      version='0.4.0',
       packages = ['msproteomicstoolslib', 
                   "msproteomicstoolslib.algorithms",
                   "msproteomicstoolslib.algorithms.alignment",
