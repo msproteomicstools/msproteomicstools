e65070c5fd3e58b18f055940013502504b26b97f
Hannes Roest <hroest@stanford.edu>
2017-07-11 13:59:56 -0700

    [FEATURE] improve script

--- analysis/fdr_estimation/ProtFDRmProphet2Mayu.py
+++ analysis/fdr_estimation/ProtFDRmProphet2Mayu.py
@@ -10,9 +10,10 @@ def handle_args():
     usage += "\nThis program converts an mProphet TSV output file to a Mayu input file"
 
     parser = argparse.ArgumentParser(description = usage )
-    parser.add_argument('--in', dest="infile", required=True, help = 'A list of mProphet output files containing all peakgroups (use quotes around the filenames)')
+    parser.add_argument('--in', dest="infile", required=True, help = 'An mProphet output file containing all peakgroups (use quotes around the filenames)')
     parser.add_argument("--out", dest="outfile", required=True, help="Output file")
     # parser.add_argument('--use_alignment_id', action='store_true', default=False, help="Use the column 'align_runid' to dermine from which run an identification originated")
+    parser.add_argument('--remove_nonunique', action='store_true', default=False, help="Remove non-unique ids")
 
     args = parser.parse_args(sys.argv[1:])
     return args
@@ -24,10 +25,13 @@ def get_filehandler(inputf):
     else:
         return open(inputf)
 
-def write_output(infile, outfile):
+def write_output(infile, outfile, remove_nonunique):
     for line_nr, line in enumerate(infile):
         runid = line[ header_dict["align_runid"] ]
         firstline = "run%s.%s.%s.%s" % (runid, line_nr, line_nr, line[ header_dict["Charge"]  ])
+        pname = line[ header_dict["ProteinName"] ]
+        if remove_nonunique and not (pname.startswith("DECOY_1/") or pname.startswith("1/")):
+            continue
         newline = [ 
             firstline,
             line[ header_dict["Sequence"]  ],
@@ -52,5 +56,5 @@ if __name__=="__main__":
     header = next(infile)
     header_dict = dict([(h, i) for i,h in enumerate(header)])
 
-    write_output(infile, outfile)
+    write_output(infile, outfile, args.remove_nonunique)
 
