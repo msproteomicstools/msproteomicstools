92f1355db1a060f3877ba16ea67be58c4d9ea701
Hannes Roest <hroest@stanford.edu>
2017-07-11 13:59:46 -0700

    [FEATURE] improve script

--- analysis/fdr_estimation/ProtFDRTraML2Fasta.py
+++ analysis/fdr_estimation/ProtFDRTraML2Fasta.py
@@ -3,9 +3,26 @@
 
 # Usage: ProtFDRTraML2Fasta.py library.tsv library.fasta
 
-import sys, csv
-infile = csv.reader(open(sys.argv[1]), delimiter="\t")
-outfile = open(sys.argv[2], "w")
+import sys, csv, argparse
+
+def handle_args():
+    usage = "" #usage: %prog --in \"files1 file2 file3 ...\" [options]" 
+    usage += "\nThis program converts a TSV spectral library to FASTA"
+
+    parser = argparse.ArgumentParser(description = usage )
+    parser.add_argument('--in', dest="infile", required=True, help = 'An OpenSWATH spectral library')
+    parser.add_argument("--out", dest="outfile", required=True, help="Output file")
+    parser.add_argument('--remove_nonunique', action='store_true', default=False, help="Remove non-unique ids (e.g. 2/id1/id2)")
+
+    args = parser.parse_args(sys.argv[1:])
+    return args
+
+args = handle_args()
+
+# infile = csv.reader(open(sys.argv[1]), delimiter="\t")
+# outfile = open(sys.argv[2], "w")
+infile = csv.reader(open(args.infile), delimiter="\t")
+outfile = open(args.outfile, "w")
 
 header = next(infile)
 header_dict = dict([(h, i) for i,h in enumerate(header)])
@@ -28,6 +45,8 @@ protein_dic = {}
 for line in infile:
     peptide = line[pepseq_pos]
     protein = line[prot_pos]
+    if args.remove_nonunique and not (protein.startswith("DECOY_1/") or protein.startswith("1/")):
+        continue
     if protein not in protein_dic:
         p = Protein(protein)
         protein_dic[protein] = p
@@ -40,3 +59,4 @@ for k in protein_dic:
     outfile.write(">%s\n" % protein.name)
     outfile.write(protein.get_concat_peptides())
     outfile.write("\n")
+
