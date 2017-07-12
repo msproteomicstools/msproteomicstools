d4212a9cba5a83b7d50a00174c2c0b4ba812e810
Hannes Roest <hroest@stanford.edu>
2017-07-12 10:18:14 -0700

    [FEATURE] wrap CyLinearInterpolate

--- msproteomicstoolslib/_optimized.pyx
+++ msproteomicstoolslib/_optimized.pyx
@@ -30,6 +30,32 @@ cdef extern from "peakgroup.h":
         double dscore_ 
         int cluster_id_
 
+cdef extern from "peakgroup.h":
+    cdef cppclass c_linear_interpolate:
+        c_linear_interpolate(libcpp_vector[double] & x, libcpp_vector[double] & y, double abs_err)
+        double predict(double xnew)
+
+
+cdef class CyLinearInterpolateWrapper:
+
+    cdef c_linear_interpolate * inst 
+
+    def __dealloc__(self):
+        del self.inst
+
+    def __init__(self, x, y, double abs_err):
+        cdef libcpp_vector[double] v1 = x
+        cdef libcpp_vector[double] v2 = y
+
+        self.inst = new c_linear_interpolate(v1, v2, abs_err)
+
+    def predict(self, list xnew):
+        ynew = []
+        cdef double x
+        for xn in xnew:
+            ynew.append( <double>deref(self.inst).predict( <double>xn) )
+        return ynew
+
 cdef class CyPeakgroupWrapperOnly:
     """
     """
