a916175b21c9bbd4bca595d20d692ea064ccb614
Hannes Roest <hroest@stanford.edu>
2017-07-13 10:28:07 -0700

    [TEST] another test

--- test/analysis_test/test_featurealignment.py
+++ test/analysis_test/test_featurealignment.py
@@ -226,5 +226,29 @@ class TestFeatureAlignment(unittest.TestCase):
         os.remove(tmpfilename)
         os.remove(tmpfilename_ids)
         os.remove(tmpfilename_matrix)
+
+    def test_9_featureAlignment_openswath_LocalMST_cython(self):
+        script = os.path.join(os.path.join(self.scriptdir, "alignment"), "feature_alignment.py")
+        filename = os.path.join(self.datadir, "feature_alignment_7_openswath_input.csv")
+        expected_outcome_ids = os.path.join(self.datadir, "feature_alignment_9_output_1_ids.csv")
+        expected_matrix_outcome = os.path.join(self.datadir, "feature_alignment_7_output_2_matrix.csv")
+        expected_outcome = os.path.join(self.datadir, "feature_alignment_8_output_3.csv")
+        tmpfilename = "featureAlignment_9.out.tmp"
+        tmpfilename_ids = "featureAlignment_9.out.tmp_idsonly.csv"
+        tmpfilename_matrix = "featureAlignment_9.out.tmp_matrix.tsv"
+
+        args = "--in %s --out %s --out_ids %s --out_matrix %s --max_rt_diff 300 --file_format openswath --max_fdr_quality 0.1 --realign_method lowess --method LocalMST --readmethod minimal --matrix_output_method RT --force" % (filename, tmpfilename, tmpfilename_ids, tmpfilename_matrix)
+
+        cmd = "python %s %s" % (script, args)
+        sub.check_output(cmd,shell=True)
+        
+        self.exact_diff(tmpfilename, expected_outcome, header_exclude = ["align_origfilename"])
+        self.exact_diff(tmpfilename_ids, expected_outcome_ids)
+        self.exact_diff(tmpfilename_matrix, expected_matrix_outcome)
+
+        os.remove(tmpfilename)
+        os.remove(tmpfilename_ids)
+        os.remove(tmpfilename_matrix)
+
 if __name__ == '__main__':
     unittest.main()
