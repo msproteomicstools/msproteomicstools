18c3d0fe321adb4736196145a041372396a81a85
Hannes Roest <hroest@stanford.edu>
2017-07-12 22:04:02 -0700

    code mv

--- msproteomicstoolslib/cython/_linear_interpol.pyx
+++ msproteomicstoolslib/cython/_linear_interpol.pyx
@@ -39,3 +39,73 @@ cdef class CyLinearInterpolateWrapper(object):
     cdef double predict_cy(self, double xnew):
         return deref(self.inst).predict(xnew)
 
+cdef class CyLightTransformationData(object):
+    """
+    A lightweight data structure to store a transformation between retention times of multiple runs.
+    """
+
+    cdef dict data 
+    cdef dict trafo 
+    cdef dict stdevs
+    cdef object reference
+
+    def __init__(self, ref=None):
+        self.data = {} 
+        self.trafo = {} 
+        self.stdevs = {} 
+        self.reference = ref
+
+    def addTrafo(self, run1, run2, CyLinearInterpolateWrapper trafo, stdev=None):
+      """
+      Add transformation between two runs
+      """
+      d = self.trafo.get(run1, {})
+      d[run2] = trafo
+      self.trafo[run1] = d
+
+      d = self.stdevs.get(run1, {})
+      d[run2] = stdev
+      self.stdevs[run1] = d
+
+    def addData(self, run1, data1, run2, data2, doSort=True):
+      """
+      Add raw data for the transformation between two runs
+      """
+      # Add data from run1 -> run2 and also run2 -> run1
+      assert len(data1) == len(data2)
+      self._doAddData(run1, data1, run2, data2, doSort)
+      self._doAddData(run2, data2, run1, data1, doSort)
+
+    def _doAddData(self, run1, data1, run2, data2, doSort):
+      if doSort and len(data1) > 0:
+          data1, data2 = zip(*sorted(zip(data1, data2)))
+      data1 = np.array(data1)
+      data2 = np.array(data2)
+      d = self.data.get(run1, {})
+      d[run2] = (data1,data2)
+      self.data[run1] = d
+
+    #
+    ## Getters
+    #
+    def getData(self, run1, run2):
+        return self.data[run1][run2]
+
+    cdef CyLinearInterpolateWrapper getTrafoCy(self, libcpp_string run1, libcpp_string run2):
+        return self.trafo[run1][run2]
+
+    def getTrafo(self, run1, run2):
+        return self.trafo[run1][run2]
+
+    def getStdev(self, run1, run2):
+        return self.stdevs[run1][run2]
+
+    cdef double getStdevCy(self, run1, run2):
+        return <double>self.stdevs[run1][run2]
+
+    def getTransformation(self, run1, run2):
+        return self.trafo[run1][run2]
+
+    def getReferenceRunID(self):
+        return self.reference
+
