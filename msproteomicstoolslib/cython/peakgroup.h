dad969ffd357f1ac8e26a215c3c4f75f88f2e4fe
Hannes Roest <hroest@stanford.edu>
2017-07-17 20:40:14 -0700

    [FIX] remove

--- msproteomicstoolslib/cython/peakgroup.h
+++ msproteomicstoolslib/cython/peakgroup.h
@@ -25,32 +25,6 @@ public:
   c_peakgroup() {};
   c_precursor* getPeptide() {return precursor;};
 
-  // copy constructor
-  c_peakgroup( const c_peakgroup &rhs)
-  {
-    // std::cout << " copy costr: c_peakgroup" << std::endl;
-    fdr_score   = rhs.fdr_score;
-    normalized_retentiontime = rhs.normalized_retentiontime;
-    internal_id_= rhs.internal_id_;  
-    intensity_  =  rhs.intensity_;    
-    dscore_     =  rhs.dscore_;       
-    cluster_id_ =  rhs.cluster_id_;   
-    precursor   =  rhs.precursor;     
-  }
-
-  c_peakgroup& operator = (const c_peakgroup &rhs)
-  {
-    // std::cout << " assignment operator : c_peakgroup" << std::endl;
-    fdr_score   = rhs.fdr_score;
-    normalized_retentiontime = rhs.normalized_retentiontime;
-    internal_id_= rhs.internal_id_;  
-    intensity_  =  rhs.intensity_;    
-    dscore_     =  rhs.dscore_;       
-    cluster_id_ =  rhs.cluster_id_;   
-    precursor   =  rhs.precursor;     
-  }
-
-
 };
 
 #endif
