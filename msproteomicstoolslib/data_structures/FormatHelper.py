8ee2244d0113b85063373a0069aa5167ba7c1cc7
Hannes Roest <hroest@stanford.edu>
2017-07-28 15:34:53 -0700

    [FIX] fix format check, add test

--- msproteomicstoolslib/data_structures/FormatHelper.py
+++ msproteomicstoolslib/data_structures/FormatHelper.py
@@ -45,27 +45,14 @@ class FormatHelper(object):
     def _compute_transitiongroup_from_key(self, key):
         """ Transforms an input chromatogram id to a string of [DECOY]_xx/yy
 
-        Possible Input Formats:
-
-        i) [DECOY_]id_xx/yy_zz
-        ii) [DECOY_]id_xx_yy
-
-        Where the DECOY_ prefix is optional, xx is the sequence and yy is the
-        charge. zz is an additional, optional annotation.
-
         We want to return only [DECOY]_xx/yy (ensuring that decoys get a
         different identifier than targets).
         """
-        components = key.split("_")
-        trgr_nr = str(components[1])
-        if components[0].startswith("DECOY"):
-            trgr_nr = "DECOY_" + str(components[2])
-
-        # Format ii) (second component doesnt contain a slash)
-        if trgr_nr.find("/") == -1:
-            trgr_nr += "/" + str(components[-1])
-
-        return trgr_nr
+        comp = self.parse(key)
+        if (comp[0]):
+            return "DECOY_" + comp[2] + "/" + comp[3]
+        else:
+            return comp[2] + "/" + comp[3]
 
     def _compute_transitiongroup_from_precursor(self, precursor):
         """ Transforms an input precursor to a string of [DECOY]_xx/yy
@@ -91,62 +78,80 @@ class FormatHelper(object):
         Checks whether the given key follows a specific format which
         could allow to map chromatograms to precursors without reading the
         whole file.
-
-        Namely, the format is expected to be [DECOY_]\d*_.*_.* from which one
-        can infer that it is openswath format.
-
-        possible inputs: 
-            DECOY_44736_NVEVIEDDKQGIIR/2_y12
-            1002781_TGLC(UniMod:4)QFEDAFTQLSGATPIGAGIDAR_3
         """
 
-        # It is probably not the openswath format with 3 or more entries
-        if len(key.split("_")) > 4:
-            return False
+        return self.parse(key) is not None
 
-        if len(key.split("_")) >= 3:
-            components = key.split("_")
-            # print "split into components", components
-            if components[0].startswith("DECOY"):
-                components = components[1:]
+    def parse(self, key_):
+        """
+        Possible Input Formats:
 
-            # Exactly 3 components are needed: id, sequence, charge/ion qualifier
-            if len(components) != 3:
-                return False
+        i) [DECOY_]id_xx/yy_zz
+        ii) [DECOY_]id_xx_yy
+        iii) [DECOY_]id_ff_cc_xx_yy
 
-            charge_is_qualifier = True
+        Where the DECOY_ prefix is optional, xx is the sequence and yy is the
+        charge. zz is an additional, optional annotation, ff is the fragment id, cc is the fragment charge.
+
+        possible inputs: 
+            1002781_TGLC(UniMod:4)QFEDAFTQLSGATPIGAGIDAR_3
+            DECOY_44736_NVEVIEDDKQGIIR/2_y12
+            DECOY_155153_GYEDPPAALFR/2_y7_2
+            DECOY_44736_y6_1_NVEVIEDDKQGIIR_2
+
+        returns tuple (decoy, trgr_nr, sequence, prec_charge, fr_id, fr_charge)
+        """
+
+        decoy = False
+        key = key_
+
+        if key.startswith("DECOY_"):
+            key = key.split("DECOY_")[1]
+            decoy = True
+
+        components = key.split("_")
+
+        fr_charge = None
+        if len(components) == 5:
             trgr_nr = components[0]
-            sequence = components[1]
+            sequence = components[3]
+            prec_charge = components[4]
+
             if len(sequence.split("/")) > 1:
                 sequence = sequence.split("/")[0]
-                charge_is_qualifier = False
-            qualifier = components[2]
 
-            if not self._is_number(trgr_nr):
-                print "Format determination: Could not convert", trgr_nr, "to int."
-                return False
-            if self._is_number(sequence):
-                print "Format determination: Does not look like sequence", sequence
-                return False
-            elif sequence.find("UniMod") != -1:
-                # Looks like sequence, has UniMod tag in it, should be ok
-                pass
+            fr_id = components[1]
+            fr_charge = components[2]
+
+        elif len(components) >= 3:
+            trgr_nr = components[0]
+            sequence = components[1]
+
+            charge_is_qualifier = True
+            if len(sequence.split("/")) > 1:
+                prec_charge = sequence.split("/")[1]
+                sequence = sequence.split("/")[0]
+                fr_id = components[2]
             else:
-                pass
-                # valid = re.match('^[A-Z]+$', sequence) is not None
-                # if not valid:
-                #     print "Format determination: Does not look like sequence", sequence
-                #     return False
+                fr_id = None
+                prec_charge = components[2]
+        else:
+            return None
 
-            if charge_is_qualifier:
-                if not self._is_number(qualifier):
-                    print "Format determination: Does not look like number", qualifier
-                    return False
-            elif self._is_number(qualifier):
-                print "Format determination: Does look like number", qualifier
-                return False
-            
-            return True
+        if not self._is_number(prec_charge):
+            print "Format determination: Precursor charge is not a number", prec_charge
+            return None
+
+        if not self._is_number(trgr_nr):
+            print "Format determination: Could not convert", trgr_nr, "to int."
+            return None
+
+        if self._is_number(sequence):
+            print "Format determination: Does not look like sequence", sequence
+            return None
+
+        return (decoy, trgr_nr, sequence, prec_charge, fr_id, fr_charge)
+
 
         # default is false
         return False
