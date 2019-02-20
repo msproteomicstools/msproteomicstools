6190b841af3607920121ff8684c2353c5268e68f
Hannes Roest <hannes.rost@utoronto.ca>
2019-02-20 13:16:38 -0500

    [NOP]

--- msproteomicstoolslib/format/SWATHScoringReader.py
+++ msproteomicstoolslib/format/SWATHScoringReader.py
@@ -192,7 +192,7 @@ class SWATHScoringReader:
                     orig_fname = this_row[header_dict[ "filename"] ]
                 current_run = Run(header, header_dict, runid, f, orig_fname, aligned_fname, useCython=useCython)
                 runs.append(current_run)
-                print(current_run, "maps to ", orig_fname)
+                print(current_run, "maps to", orig_fname)
             else: 
                 assert len(current_run) == 1
                 current_run = current_run[0]
